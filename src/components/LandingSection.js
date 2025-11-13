7e002ca632ec2ca0e5185742316e385f7fab8a53
rain_ <rhinezen@gmail.com>
2025-11-13 20:37:07 +0100

    using a local image now

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
+import profileImageSrc from "../images/profile_pic.jpg";
 
 const greeting = "Hello, I am Adri!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
-const profileImageSrc = "https://media.licdn.com/dms/image/v2/D4D03AQEAtsv798dDLQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1713815782783?e=1764806400&v=beta&t=4S2Afr98-EUI3T7XjrbhXhY38jzYkUjPdRLQkzcW6IY";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
